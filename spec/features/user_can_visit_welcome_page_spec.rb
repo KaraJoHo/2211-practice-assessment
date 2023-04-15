225ffbe4d636dc5ef0bb46e9a1e5c0d67b22bb6a
KaraJH <karas.jones@yahoo.com>
2023-04-15 16:53:36 -0600

    test: Redirect to search after clicking from dropdown

--- spec/features/user_can_visit_welcome_page_spec.rb
+++ spec/features/user_can_visit_welcome_page_spec.rb
@@ -7,4 +7,12 @@ describe "user can visit the welcome page" do
     expect(page).to have_content("Search For The Nearest Electric Charging Station")
     expect(page).to have_button("Find Nearest Station")
   end
+
+  scenario "can select from the drop down menu and click find to be redirected to a search page" do 
+    visit "/"
+    select "Griffin Coffe", from: :location
+    click_on("Find Nearest Station")
+
+    expect(current_path).to eq("/search")
+  end
 end
